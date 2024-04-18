c45ea494c385a24d253ffc693c106827781dc54b
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2024-04-19 00:41:04 +0300

    responsive design for other services

--- figma-task-c/src/app/components/OtherServices.tsx
+++ figma-task-c/src/app/components/OtherServices.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const OtherServices: React.FC = () => {
 	return (
 		<div className='my-20 mx-auto'>
-			<h1 className='text-4xl text-gray-800 font-semibold mb-14'>Други услуги</h1>
+			<h1 className='text-4xl text-gray-800 font-semibold mb-14 text-center md:text-left md:pl-6'>Други услуги</h1>
 
-			<div className='flex flex-row space-x-6 text-lg'>
+			<div className='flex flex-col items-center space-y-6 md:flex-row md:justify-center md:space-x-6 md:space-y-0 text-lg px-6'>
 				<div>
 					<img src='/1.png' alt='card-img1' className='rounded-2xl mb-6' />
 					<span className='font-semibold pl-2'>Опасни товари (ADR)</span>
